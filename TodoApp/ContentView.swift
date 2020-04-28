9d488a741985f4d04556d1fe58e0d9d6eadbd907
Tanaka Koki <tanaka@bp.i3-systems.com>
2020-04-28 11:53:43 +0900

    Initial Commit

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Koki Tanaka on 2020/04/28.
+//  Copyright © 2020 Koki Tanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
